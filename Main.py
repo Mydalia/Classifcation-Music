7876ffa6279f29713bba7e580ac67546f5294bb9
Mydalia <77779115+Mydalia@users.noreply.github.com>
2022-01-13 11:25:22 +0100

    Affichage du csv + Modification du load_spotify_dataset

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ from IPython.display import display
 from sklearn.utils import Bunch
 
 def load_spotify_dataset():
-    with open(r'/Ressources/spotify_genre_final.csv') as csv_file:
+    with open(r'./Ressources/spotify_genre_final.csv') as csv_file:
         data_reader = csv.reader(csv_file)
         feature_names = next(data_reader)[:-1]
         data = []
@@ -15,9 +15,18 @@ def load_spotify_dataset():
         for row in data_reader:
             features = row[:-1]
             label = row[-1]
-            data.append([float(num) for num in features])
-            target.append(int(label))
+            data.append(features)
+            target.append(label)
         
         data = np.array(data)
         target = np.array(target)
     return Bunch(data=data, target=target, feature_names=feature_names)
+
+mfd = load_spotify_dataset()
+
+X = mfd.data
+
+y = mfd.target
+
+print(X)
+print(y)
